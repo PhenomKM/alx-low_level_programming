43bcb2ab762dab88c7660703219fb7b5baf8fce7
Kevin Muema <kevin.m.muema@gmail.com>
2022-09-08 23:32:12 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints using the puts function.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
