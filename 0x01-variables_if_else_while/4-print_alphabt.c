9c5a8d5c7c448f1d113a7064d6bb810c8955ed1a
Kevin Muema <kevin.m.muema@gmail.com>
2022-09-09 16:04:46 +0000

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print the alphabet omitting e and q
+ * using putchar twice only
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+		if (ch == 'e' || ch == 'q')
+			ch++;
+	}
+	putchar('\n')
+	
+	return (0);
+}
