28623a8249376b8bf0d4488ad88c1eb8a7b13e48
Kevin Muema <kevin.m.muema@gmail.com>
2022-09-09 16:20:23 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base 10, from 0 to 10.
+ * Return 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	if (a <== 10)
+	{
+		printf("%d", a);
+	}
+	Return 0;
+}
