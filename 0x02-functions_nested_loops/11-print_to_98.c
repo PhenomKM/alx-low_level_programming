b100038e0defdda37ffeaf5aae162965e49a2b24
Kevin Muema <kevin.m.muema@gmail.com>
2022-09-13 23:58:48 +0000

    Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,52 +1,22 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * print_below_98 - check for numbers below 98
- * @a: int type
- *
- * Return: return all values including 98
- */
-void print_below_98(int a)
-{
-	int c;
-
-	for (c = a; c <= 98; c++)
-	{
-		if (c != 98)
-			printf("%d, ", c);
-		else
-			printf("%d\n", c);
-	}
-}
-
-/**
- * print_above_98 - check for numbers above 98
- * @d: int type
- *
- * Return: return all values including 98
- */
-void print_above_98(int d)
-{
-	int b;
-
-	for (b = d;  b >= 98; b--)
-	{
-		if (b != 98)
-			printf("%d, ", b);
-		else
-			printf("%d\n", b);
-	}
-}
-
-/**
- * print_to_98 - check for numbers below 98
- * @n: int type
- *
+ * print_to_98 - Prints all natural numbers from input to 98
+ * separated by a comma followed by a space.
+ * @n: The number to begin counting at
  * Return: return all values including 98
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
-		print_below_98(n);
-	else
-		print_above_98(n);
+		if (n >= 98)
+		{
+			while (n > 98)
+				printf("%d, ", n--);
+			printf("%d\n", n);
+		}
+		else
+		{
+			while (n < 98)
+				printf("%d, ", n++);
+			printf("%d\n", n);
+		}
 }
