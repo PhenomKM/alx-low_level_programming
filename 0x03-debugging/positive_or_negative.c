015d731e1c1dab9922cee3798642f24c5d9f4a38
Kevin Muema <kevin.m.muema@gmail.com>
2022-09-15 12:20:46 +0000

    positive or negative file

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,14 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - Prints out random sizes of various data.
+ * Description: using the main function
+ * this program prints a statement.
+ * Return: 0 if success
+ */
 int main(void)
 {
-		int n;
+	int n;
 
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-		/* your code goes there */
-		return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+	return (0);
 }
