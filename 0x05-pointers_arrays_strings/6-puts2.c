ae57dbbe0903f8140b4738b94f2d045f07a540fb
Kevin Muema <kevin.m.muema@gmail.com>
2022-09-19 23:12:56 +0000

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - print every second character of string
+ * @str: string to print characters from
+ */
+void puts2(char *str)
+{
+	if (*str == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	do {
+		_putchar(*str);
+		if (!*(str + 1))
+			break;
+		str += 2;
+	} while (*str);
+	_putchar('\n');
+}
