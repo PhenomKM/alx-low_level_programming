5040616174e6fbd84a9f02868257887097c2cf6c
Kevin Muema <kevin.m.muema@gmail.com>
2022-09-23 01:02:28 +0000

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_number - prints a number
@@ -9,11 +9,13 @@
 void print_number(int n)
 {
 	int res, temp, expo;
-
+	
 	expo = 1;
 /*Check negatives*/
 	if (n >= 0)
+	{
 		res = n * -1;
+	}
 	else
 	{
 		res = n;
@@ -26,10 +28,13 @@ void print_number(int n)
 	{
 		expo *= 10;
 		temp /= 10;
+		printf("expo: %d, temp: %d, orig: %d\n", expo, temp, res);
 	}
 /*Main */
 	while (expo >= 1)
 	{
+
+		printf("expo: %d, res: %d\n", expo, ((res /expo) % 10) * -1);
 		_putchar(((res / expo) % 10) * -1 + '0');
 		expo /= 10;
 	}
