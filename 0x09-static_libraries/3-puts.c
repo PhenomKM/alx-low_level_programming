e9584297b03341b7a1ea006d23b9e9ae4410c605
Kevin Muema <kevin.m.muema@gmail.com>
2022-09-30 19:12:22 +0000

    edited one file

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _puts - print string and newline
- * @str: string to print
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
  */
 void _puts(char *str)
 {
-	while (*str)
-	{
-		str++;
-	}
-}
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
