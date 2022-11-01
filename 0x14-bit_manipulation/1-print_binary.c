3602b7e19b2c47f110a4e555524d5419dcd97a16
Kevin Muema <kevin.m.muema@gmail.com>
2022-11-01 03:14:42 +0000

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,33 +1,22 @@
 #include "main.h"
+
 /**
- * print_binary - Entry Point
- * @n: dec input
- * Return: 0
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
  */
 void print_binary(unsigned long int n)
 {
-	int i = 0, count, k, temp;
-
-	if (n == 0)
+	if (n >> 0)
 	{
-		printf("0");
-		return;
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-
-	temp = n;
-
-	while (temp != 0)
+	else
 	{
-		i++;
-		temp = temp >> 1;
-	}
-
-	for (count = i - 1; count >= 0; count--)
-	{
-		k = n >> count;
-		if (k & 1)
-			printf("1");
-		else
-			printf("0");
+		_putchar('0');
 	}
 }
